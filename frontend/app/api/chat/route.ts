9ce9daaf93bb79e89aed90d21d7a0d7d9d323a98
Rohith-Raju <rohithraju488@gmail.com>
2025-04-04 13:46:34 +0200

    update: added chat functionality
    Signed-off-by: Rohith-Raju <rohithraju488@gmail.com>

--- frontend/app/api/chat/route.ts
+++ frontend/app/api/chat/route.ts
@@ -1,4 +1,4 @@
-import { openai } from "@ai-sdk/openai";
+import { google } from "@ai-sdk/google";
 import { jsonSchema, streamText } from "ai";
 
 export const maxDuration = 30;
@@ -7,14 +7,14 @@ export async function POST(req: Request) {
   const { messages, system, tools } = await req.json();
 
   const result = streamText({
-    model: openai("gpt-4o"),
+    model: google("gemini-2.0-flash-001"),
     messages,
     system,
     tools: Object.fromEntries(
       Object.keys(tools).map((name) => [
         name,
         { ...tools[name], parameters: jsonSchema(tools[name].parameters) },
-      ])
+      ]),
     ),
   });
 
