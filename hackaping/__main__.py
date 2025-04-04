d02fbb5948b895cf8a1f1091bf60e22be1c7e591
Axel Gard <axel.gard@protonmail.com>
2025-04-04 15:47:50 +0200

    added basic sim, only for time optim

--- hackaping/__main__.py
+++ hackaping/__main__.py
@@ -1,24 +1,35 @@
-class Item:
-    def __init__(self, val:int=0) -> None:
-        self.val = val
+import typing
 
-    def __str__(self):
-        return str(self.val)
 
-    def __repr__(self) -> str: 
-        return str(self.val)
+def simulation_one_machine(proc_time:float, demands:int, switching_cost:float): 
+    return proc_time * demands + switching_cost
 
-class Transformer:
-    def __init__(self) -> None:
-        self.items = [] 
+#def simulation_one_machine(proc_times:typing.List[float], demands:typing.List[int], switching_cost:float): 
+#    return sum([proc_times[i] * demands[i] + switching_cost for i in range(len(proc_times))])
 
+def simulation_machines(proc_times:typing.List[float], demands:typing.List[int], switching_cost:float, n_machines:int):
+    #number_of_runs = len(proc_times) + (len(proc_times) % n_machines)
+    return sum([proc_times[i] * demands[i] + switching_cost for i in range(len(proc_times))]) / n_machines
 
 def main():
-    machine_1 = Transformer()
-    for i in range(10):
-        machine_1.items.append(Item(val=i))
+    switching_cost = 20.0
 
-    print(f"current items = {machine_1.items}")
+    proc_times = [1.0, 1.0, 1.0]
+    demands = [10, 10, 10]
+    n_machines = 2
+    print("Simulation with one machine:")
+    print(simulation_one_machine(proc_times, demands, switching_cost))
+    print("Simulation with multiple machines:")
+    print(simulation_machines(proc_times, demands, switching_cost, n_machines))
+
+    
+
+    
+
+
+
+
 
 if __name__ == "__main__":
     main()
+
--- hackaping/lambda_function.py
+++ hackaping/lambda_function.py
@@ -1,8 +1,19 @@
 import json
+import typing
+
+def simulation_one_machine(proc_time:float, demands:int, switching_cost:float): 
+    return proc_time * demands + switching_cost
+
+def simulation_machines(proc_times:typing.List[float], demands:typing.List[int], switching_cost:float, n_machines:int):
+    return sum([proc_times[i] * demands[i] + switching_cost for i in range(len(proc_times))]) / n_machines
 
 def lambda_handler(event, context):
-    # TODO implement
+    body = json.loads(event['body'])
+    time =  simulation_machines(body["process_time"], body["demands"], 20.0, body["n_machines"]) 
+
     return {
         'statusCode': 200,
-        'body': json.dumps('Hello from Lambda!')
+        'body': json.dumps({
+            "time": time,
+        })
     }
